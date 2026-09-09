68ff3c436371df45531555c2e05d4a7868b72598
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2026-09-09 16:31:29 +0100

    Creating Member class

---
+++ models/member.py
@@ -0,0 +1,15 @@
+class Member:
+    def __init__(self, member_id, name, email):
+        self.member_id = member_id
+        self.name = name
+        self.email = email
+        self.borrowed_books = []
+
+    def display_info(self):
+        print(f"Member ID: {self.member_id}")
+        print(f"Name: {self.name}")
+        print(f"Email: {self.email}")
+        print(f"Borrowed Books: {self.borrowed_books}")
+
+member1 = Member(1, "Anwar", "anwarsagirmustapha1@gmail.com")
+member1.display_info()
