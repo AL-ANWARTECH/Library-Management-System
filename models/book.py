8d328f01679a15dc36682d0abdcc6e579a4538a2
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2026-09-09 15:54:08 +0100

    Creating Directories and Class Book

---
+++ models/book.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, book_id, title, author):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.is_available = True
+
+    def display(self):
+        print(f"Book ID: {self.book_id}")
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Available: {self.is_available}")
+
+book1 = Book(1, "Python Crash Course", "Eric Matthes")
+
+book1.display()
