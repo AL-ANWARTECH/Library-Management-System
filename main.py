d59678da711a6da4fac85b007c5f7eff3c3ba87e
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2026-09-13 11:28:39 +0100

    feat: add robust input validation

--- main.py
+++ main.py
@@ -5,127 +5,172 @@ from services.library import Library
 def main():
     library = Library()
 
-
-    # =========================
-    # ADD BOOKS
-    # =========================
-
-    book1 = Book(1, "Python Crash Course", "Eric Matthes")
-    book2 = Book(2, "Django for Beginners", "Mosh")
-
-    library.add_book(book1)
-    library.add_book(book2)
-
-    library.display_books()
-
-
-    # =========================
-    # REGISTER MEMBERS
-    # =========================
-
-    member1 = Member(1, "Anwar", "anwarsagirmustapha1@gmail.com")
-    member2 = Member(2, "Hafsat", "hafsatmukthar@gmail.com")
-    try:
-        invalid_member = Member(5, "Test User", "invalid-email")
-    except ValueError as error:
-        print(f"Error: {error}")
-
-    library.register_member(member1)
-    library.register_member(member2)
-
-    library.search_member_by_id(1)
-    library.search_member_by_id(5)
-
-    library.search_member_by_name("anwar")
-    library.search_member_by_name("xyz")
+    while True:
+        print("\n===== LIBRARY MANAGEMENT SYSTEM =====")
+        print("1. Add Book")
+        print("2. Register Member")
+        print("3. Display Books")
+        print("4. Display Members")
+        print("5. Borrow Book")
+        print("6. Return Book")
+        print("7. Search Book")
+        print("8. Search Member")
+        print("9. Exit")
 
 
-    # =========================
-    # BORROW BOOK
-    # =========================
+        choice = input("Enter your choice: ")
 
-    library.borrow_book(member1, book1)
+        if choice == "1":
+            try:
+                book_id = int(input("Enter book ID: "))
+                title = input("Enter book title: ")
+                author = input("Enter book author: ")
 
-    library.display_books()
-    library.display_members()
+                book = Book(book_id, title, author)
+                library.add_book(book)
 
-
-    # =========================
-    # TEST BORROWING UNAVAILABLE BOOK
-    # =========================
-
-    library.borrow_book(member2, book1)
+            except ValueError:
+                print("Invalid book ID. please enter a number.")
 
 
-    # =========================
-    # RETURN BOOK
-    # =========================
+        elif choice == "2":
+            try:
 
-    library.return_book(member1, book1)
+                member_id = int(input("Enter member ID: "))
+            except ValueError:
+                print("Invalid member ID. Please enter a number.")
+                continue
 
-    library.return_book(member2, book1)
+            name = input("Enter member name: ")
+            email = input("Enter member email: ")
 
+            try:
+                member = Member(member_id, name, email)
+                library.register_member(member)
+            except ValueError as error:
+                print(f"Error: {error}")
 
-    # =========================
-    # SEARCH BY TITLE
-    # =========================
+        elif choice == "3":
+            library.display_books()
 
-    library.search_by_title("Python")
-    library.search_by_title("PYTHON CRASH COURSE")
-    library.search_by_title("Clean Code")
+        elif choice == "4":
+            library.display_members()
 
+        elif choice == "5":
+            try:
+                member_id = int(input("Enter member ID: "))
+                book_id = int(input("Enter book ID: "))
+            except ValueError:
+                print("Invalid ID. Please enter numbers only. ")
+                continue
 
-    # =========================
-    # SEARCH BY AUTHOR
-    # =========================
-    library.search_by_author("Eric")
-    library.search_by_author("eric matthes")
-    library.search_by_author("ERIC MATTHES")
-    library.search_by_author("Robert Martin")
+            for member in library.members:
+                if member.member_id == member_id:
+                    selected_member = member
+                    break
+            else:
+                selected_member = None
 
+            for book in library.books:
+                if book.book_id == book_id:
+                    selected_book = book
+                    break
 
-    # =========================
-    # SEARCH BY ID
-    # =========================
-    try: 
-       book_id = int(input("Enter book ID to search: "))
-       library.search_by_id(book_id)
-    except ValueError:
-        print("Invalid book ID. Please enter a number.")
-    library.search_by_id(2)
-    library.search_by_id(5)
+            else:
+                selected_book = None
 
+            if selected_member is None:
+                print("Member not found.")
+            elif selected_book is None:
+                print("Book not found.")
+            else:
+                library.borrow_book(selected_member, selected_book)
 
-    # =========================
-    # TEST DUPLICATE BOOK
-    # =========================
+        elif choice == "6":
+            try:
+                member_id = int(input("Enter member ID: "))
+                book_id = int(input("Enter book ID: "))
+            except ValueError:
+                print("Invalid ID. Please enter numbers only.")
+                continue
 
-    book3 = Book(1, "Clean Code", "Robert Martin")
-    library.add_book(book3)
+            for member in library.members:
+                if member.member_id == member_id:
+                    selected_member = member
+                    break
+            else:
+                selected_member = None
 
+            for book in library.books:
+                if book.book_id == book_id:
+                    selected_book = book
+                    break
+            else:
+                selected_book = None
 
-    # =========================
-    # TEST DUPLICATE MEMBER
-    # =========================
+            if selected_member is None:
+                print("Member not found.")
+            elif selected_book is None:
+                print("Book not found.")
+            else:
+                library.return_book(selected_member, selected_book)
 
-    member3 = Member(1, "Abdullahi", "abdullahiyakubabo@gmail.com")
-    library.register_member(member3)
+        elif choice == "7":
+            print("\n--- SEARCH BOOK ---")
+            print("1. Search by Title")
+            print("2. Search by Author")
+            print("3. Search by ID")
 
+            search_choice = input("Enter your choice: ")
 
-    # =========================
-    # TEST UNREGISTERED BOOK
-    # =========================
+            if search_choice == "1":
+                title = input("Enter book title: ")
+                library.search_by_title(title)
 
-    book4 = Book(4, "Clean Architecture", "Robert Martin")
-    library.borrow_book(member1, book4)
+            elif search_choice == "2":
+                author = input("Enter book author: ")
+                library.search_by_author(author)
 
+            elif search_choice == "3":
+                try:
+                    book_id = int(input("Enter book ID: "))
+                    library.search_by_id(book_id)
+                except ValueError:
+                    print("Invalid book ID. Please enter a number.")
 
-    # =========================
-    # TEST UNREGISTERED MEMBER
-    # =========================
+            else:
+                print("Invalid search option.")
 
-    member4 = Member(4, "Ali", "ali@gmail.com")
-    library.return_book(member4, book1)
+        elif choice == "8":
+            print("\n--- SEARCH MEMBER ---")
+            print("1. Search by ID")
+            print("2. Search by Name")
+
+            search_choice = input("Enter your choice: ")
+
+            if search_choice == "1":
+                try:
+                    member_id = int(input("Enter member ID: "))
+                    library.search_member_by_id(member_id)
+                except ValueError:
+                    print("Invalid member ID. Please enter a number.")
+
+            elif search_choice == "2":
+                name = input("Enter member name: ")
+                library.search_member_by_name(name)
+
+            else:
+                print("Invalid search option.")
+
+        elif choice == "9":
+            print("Thank you for using the Library Management System.")
+            break
+
+        
+        else:
+            print("Invalid input.")
+
+        
 
 if __name__ == "__main__":
     main()
